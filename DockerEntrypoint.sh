25d71e084e1332b02a50464519272a84ca87dcdf
dffgdffghgfhfh <liangjune.hu@gmail.com>
2025-02-08 20:04:27 +0800

    Update DockerEntrypoint.sh

--- DockerEntrypoint.sh
+++ DockerEntrypoint.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/sh 
 
-# Start fail2ban
+# Start fail2ban (如果启用了 fail2ban)
 [ $X_UI_ENABLE_FAIL2BAN == "true" ] && fail2ban-client -x start
+
+# Mount rclone love: remote storage to /mnt
+rclone mount love: /mnt --allow-other --vfs-cache-mode writes &
 
 # Run x-ui
 exec /app/x-ui
+
